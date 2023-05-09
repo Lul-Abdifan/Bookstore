5681cf99cb5aafaaede2f2ef4cbd3d8c4cfc149c
Lul-Abdifan <tasanbannu@gmail.com>
2023-05-09 08:51:53 -0400

    Create redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,1 @@
-import { configureStore } from "@reduxjs/toolkit";
-import booksReducer from "./features/books/booksSlice";
-import categoriesReducer from "./features/categories/categoriesSlice";
 
-const store = configureStore({
-  reducer: {
-    book: booksReducer,
-    categories: categoriesReducer,
-  },
-});
-
-export default store;
