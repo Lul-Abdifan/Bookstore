2a0f8791a597f2807872cbe9b977c44337d8e6e1
Lul-Abdifan <tasanbannu@gmail.com>
2023-05-12 10:39:32 -0400

    Update category element

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
-import { removeBook, deleteBook } from '../redux/features/books/booksSlice';
+import React from "react";
+import PropTypes from "prop-types";
+import { useDispatch } from "react-redux";
+import { removeBook, deleteBook } from "../redux/features/books/booksSlice";
 
-function Book({
-  itemId, title, author, category,
-}) {
+function Book({ itemId, title, author, category }) {
+  const dispatch = useDispatch();
 
+  const remover = (itemId) => {
+    dispatch(deleteBook(itemId)).then(() => dispatch(removeBook(itemId)));
+  };
   return (
     <div>
       <p>{category}</p>
       <h2>{title}</h2>
       <p>{author}</p>
 
-      <button
-        type="submit"
-        onClick={() => remover(itemId)}
-      >
+      <button type="submit" onClick={() => remover(itemId)}>
         Delete
       </button>
     </div>
