fcc4cbd7f4e3dcbc7ea9b76318b3512ee3d08cbd
Lul-Abdifan <tasanbannu@gmail.com>
2023-05-12 10:32:48 -0400

    Update the Form to connect with API

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,13 +4,7 @@ import { useDispatch } from 'react-redux';
 import { addBook, postBook } from '../redux/features/books/booksSlice';
 
 function Form() {
-  const categories = ['Fiction', 'Novel', 'Education'];
-  const dispatch = useDispatch();
-  const [book, setBook] = useState({
-    title: '',
-    author: '',
-    category: '',
-  });
+ 
 
   const addBookForm = (event) => {
     event.preventDefault();
